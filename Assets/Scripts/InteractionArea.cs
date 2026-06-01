accda3d635b946486cb7c083a5b3fa5b79ab2426
JuanBautista2009 <j.bautista.godoy@gmail.com>
2026-06-01 14:24:31 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public int score = 0;
+    void Start()
+    {
+         
+    }
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col){
+        if (col.gameObject.CompareTag("Pickable")){
+            Destroy(col.gameObject);
+            score ++;
+            Debug.Log("Puntaje: " + score);
+        }
+    }
+}
