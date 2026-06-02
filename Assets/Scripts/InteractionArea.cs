5179639714b7d2c4dd8d04249d2458b1f4bedd44
klaztor <marcoferrariserber@gmail.com>
2026-06-01 21:36:34 -0300

    PuntajeUGUI

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InteractionArea : MonoBehaviour
 {
+    public TextMeshProUGUI puntaje;
     public int score = 0;
     void Start()
     {
-         
+         puntaje.gameObject.SetActive(false);
     }
     void Update()
     {
-        
+        puntaje.gameObject.SetActive(true);
+        puntaje.text = "Score" + ":" + score.ToString();
     }
     void OnTriggerEnter(Collider col){
         if (col.gameObject.CompareTag("Pickable")){
