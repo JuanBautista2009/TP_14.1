b93f6329b626f92719a4eca629b34b1c7f15fa94
JuanBautista2009 <j.bautista.godoy@gmail.com>
2026-06-08 13:43:28 -0300

    Update InteractionArea.cs

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -7,26 +7,42 @@ public class InteractionArea : MonoBehaviour
 {
     public TextMeshProUGUI puntaje;
     public TextMeshProUGUI timer;
-    private float tiempoTranscurrido = 0f;
+    
+    [SerializeField] private float tiempoRestante = 120f; 
     public int score = 0;
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-       
-        puntaje.text = "Score" + ":" + score.ToString();
-        tiempoTranscurrido += Time.deltaTime;
-        int minutos = Mathf.FloorToInt(tiempoTranscurrido / 60);
-        int segundos = Mathf.FloorToInt(tiempoTranscurrido % 60);
+
+    void Update(){
+        int minutos = Mathf.FloorToInt(tiempoRestante / 60);
+        int segundos = Mathf.FloorToInt(tiempoRestante % 60);
         timer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+        puntaje.text = "Score: " + score.ToString();
+
+        if (tiempoRestante > 0)
+        {
+            tiempoRestante -= Time.deltaTime;
+
+            if (tiempoRestante < 0)
+            {
+                tiempoRestante = 0;
+                TerminarJuego();
+            }
+        }
     }
+
     void OnTriggerEnter(Collider col){
-        if (col.gameObject.CompareTag("Pickable")){
+        if (col.gameObject.CompareTag("Pickable"))
+        {
             Destroy(col.gameObject);
-            score ++;
+            score++;
             Debug.Log("Puntaje: " + score);
         }
+    }
+
+    void GameOver(){
+        Debug.Log("¡Tiempo agotado!");
+    }
+
+    void YouWon(){
+
     }
 }
