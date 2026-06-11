a2514d81d68692aa73878af4074e194ad0f2736a
JuanBautista2009 <j.bautista.godoy@gmail.com>
2026-06-11 13:01:14 -0300

    Terminado

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -1,58 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class InteractionArea : MonoBehaviour
 {
-    public TextMeshProUGUI puntaje;
-    public TextMeshProUGUI timer;
-
-    public TextMeshProUGUI YouWon;
-    public TextMeshProUGUI GameOver;
-
-    [SerializeField] private float tiempoRestante = 120f;
-    public int score = 0;
+    private UIManager uiManager;
 
     void Start()
     {
-        YouWon.gameObject.SetActive(false);
-        GameOver.gameObject.SetActive(false);
-    }
-
-    void Update()
-    {
-        int minutos = Mathf.FloorToInt(tiempoRestante / 60);
-        int segundos = Mathf.FloorToInt(tiempoRestante % 60);
-
-        timer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
-        puntaje.text = "Score: " + score.ToString();
-
-        if (score >= 7)
-        {
-            YouWon.gameObject.SetActive(true);
-            return;
-        }
-
-        if (tiempoRestante > 0)
-        {
-            tiempoRestante -= Time.deltaTime;
-
-            if (tiempoRestante < 0)
-            {
-                tiempoRestante = 0;
-                GameOver.gameObject.SetActive(true);
-            }
-        }
+        uiManager = FindObjectOfType<UIManager>();
     }
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.CompareTag("Pickable"))
-        {
+        if (col.gameObject.CompareTag("Pickable")){
             Destroy(col.gameObject);
-            score++;
-            Debug.Log("Puntaje: " + score);
+            uiManager.SumarPunto();
         }
     }
 }
--- Assets/Scripts/UIMANAGER.cs
+++ Assets/Scripts/UIMANAGER.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class UIMANAGER : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI puntaje;
+    public TextMeshProUGUI timer;
+    public TextMeshProUGUI YouWon;
+    public TextMeshProUGUI GameOver;
+
+    private float tiempoRestante = 120f;
+    private int score = 0;
+    private bool juegoTerminado = false;
+
     void Start()
     {
-        
+        YouWon.gameObject.SetActive(false);
+        GameOver.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (juegoTerminado) return;
+
+        if (tiempoRestante > 0){
+            tiempoRestante -= Time.deltaTime;
+        }
+        else{
+            tiempoRestante = 0;
+            juegoTerminado = true;
+            GameOver.gameObject.SetActive(true);
+        }
+
+        int minutos = Mathf.FloorToInt(tiempoRestante / 60);
+        int segundos = Mathf.FloorToInt(tiempoRestante % 60);
+
+        timer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+        puntaje.text = "Score: " + score.ToString();
+    }
+
+    public void SumarPunto()
+    {
+        if (juegoTerminado) return;
+
+        score++;
+
+        if (score >= 7){
+            juegoTerminado = true;
+            YouWon.gameObject.SetActive(true);
+        }
     }
 }
